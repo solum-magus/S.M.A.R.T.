03979de7c3be0cd5273a10aebe7f7264e9dad150
solum-magus <stephenchase728@gmail.com>
2024-11-16 02:23:13 +0800

    Update and rename Homepage.html to Homepage.php

--- Pages/Homepage.php
+++ Pages/Homepage.php
@@ -1,3 +1,27 @@
+<?php
+
+    session_start();
+
+    if (isset($_SESSION["fname"]) && isset($_SESSION["position"])) {
+
+        $mysqli = require __DIR__ . "/../database.php";
+
+        $fname = $mysqli->real_escape_string($_SESSION["fname"]);
+        $position = $mysqli->real_escape_string($_SESSION["position"]);
+
+        $sql = "SELECT * FROM userinfo
+                WHERE full_name = '$fname'
+                AND position = '$position'";
+
+        $result = $mysqli->query($sql);
+
+        $user = $result->fetch_assoc();
+
+    }
+
+?>
+
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,19 +37,22 @@
     <body>
         <div class="Container">
             <nav>
-                <a href="Homepage.html">
-                    <svg id="logo" height="50" width="50">
-                        <image height="50" width="50" href="../Assets/Logo2.png"></image>
-                    </svg>
-                    <span id="smart">SMART</span>
-                </a>
+                <svg id="logo" height="50" width="50">
+                    <image height="50" width="50" href="../Assets/Logo2.png"></image>
+                </svg>
+                <span id="smart">SMART</span>
             </nav>
 
             <main>
                 <div id="homeprof">
                     <div id="prof22"><img id="prof2" src="../Assets/Profile.png"></div>
-                    <div id="nameposition"><span>Placeholder Placeholder Placeholder</span>
-                    <span>(Position)</span></div>
+
+                    <?php if (isset($fname)):  ?>
+
+                    <div id="nameposition"><span><?= htmlspecialchars($user["full_name"]) ?></span>
+                    <span><?= htmlspecialchars($user["position"]) ?></span></div>
+
+                    <?php endif; ?>
                 </div>
 
                 <div id="report">
@@ -58,11 +85,11 @@
 
             <div id="side">
                 <div id="icons">
-                    <a href="Profile.html"><img id="prof" src="../Assets/Profile.png"></a>
-                    <a href="Homepage.html"><img id="home" src="../Assets/Home.png"></a>
-                    <a href="History.html"><img id="id" src="../Assets/History.png"></a>
-                    <a href="Feedback.html"><img id="fb" src="../Assets/Feedback.png"></a>
-                    <a href="Notification.html"><img id="notif" src="../Assets/Notification.png"></a>
+                    <a href="Profile.php"><img id="prof" src="../Assets/Profile.png"></a>
+                    <a href="Homepage.php"><img id="home" src="../Assets/Home.png"></a>
+                    <a href="History.php"><img id="id" src="../Assets/History.png"></a>
+                    <a href="Feedback.php"><img id="fb" src="../Assets/Feedback.png"></a>
+                    <a href="Notification.php"><img id="notif" src="../Assets/Notification.png"></a>
                 </div>
             </div>
         </div>
