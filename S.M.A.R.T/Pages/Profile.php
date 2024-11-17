5f77af8f14020a824642969e6d69d0fc0fbdf515
solum-magus <stephenchase728@gmail.com>
2024-11-17 22:57:57 +0800

    Update Profile.php

--- S.M.A.R.T/Pages/Profile.php
+++ S.M.A.R.T/Pages/Profile.php
@@ -2,16 +2,20 @@
 
     session_start();
 
-    if (isset($_SESSION["fname"]) && isset($_SESSION["position"])) {
+    if (isset($_SESSION["fname"]) && isset($_SESSION["position"]) && isset($_SESSION["id"]) && isset($_SESSION["bio"])) {
 
         $mysqli = require __DIR__ . "/../database.php";
 
         $fname = $mysqli->real_escape_string($_SESSION["fname"]);
         $position = $mysqli->real_escape_string($_SESSION["position"]);
+        $id = $mysqli->real_escape_string($_SESSION["id"]);
+        $bio = $mysqli->real_escape_string($_SESSION["bio"]);
 
         $sql = "SELECT * FROM userinfo
                 WHERE full_name = '$fname'
-                AND position = '$position'";
+                AND position = '$position'
+                AND school_id = '$id'
+                AND bio = '$bio'";
 
         $result = $mysqli->query($sql);
 
@@ -48,18 +52,43 @@
                 <div class="profile-container">
                     <div class="profile-header">
                         <img class="profile-picture" src="../Assets/Profile.png" alt="Profile Picture">
-                        <h1 class="profile-name">Placeholder</h1>
-                        <p class="profile-position">(Position)</p>
+
+                        <?php if (isset($fname) && isset($position)):  ?>
+
+                        <h1 class="profile-name"><?= htmlspecialchars($user["full_name"]) ?></h1>
+
+                        <p class="profile-position"><?= htmlspecialchars($user["position"]) ?></p>
+
+                        <?php else: ?>
+
+                        <h1 class="profile-name">???<h1>
+
+                        <p class="profile-position">???</p>
+
+                        <?php endif; ?>
                     </div>
             
                 <div class="profile-details">
                     <h2>About</h2>
-                    <p>Bio</p>
+
+                    
+                    <?php if (isset($id) && isset($bio)):  ?>
+
+                    <h1 class="profile-name"><?= htmlspecialchars($user["school_id"]) ?></h1>
+
+                    <p class="profile-position"><?= htmlspecialchars($user["bio"]) ?></p>
+
+                    <?php else: ?>
+
+                    <p>???</p>
+                    <p>???</p>
+
+                    <?php endif; ?>
 
                     <h2>Recent Activity</h2>
                     <ul>
-                        <li>Reported issue in Room 101 - 10/11/2024</li>
-                        <li>Submitted feedback - 08/10/2024</li>
+                        <li>PLACEHOLDER issue in Room 101 - 10/11/2024</li>
+                        <li>PLACEHOLDER feedback - 08/10/2024</li>
                     </ul>
                 </div>
 
@@ -67,18 +96,26 @@
                     <a href="../Authentication/signout.php"><button>Sign out</button></a>                      
                 </div>
 
-                <form action="../Authentication/change_pass.php" method="POST">
+                <form id="ChangePasswordForm" action="../Authentication/change_pass.php" method="POST">
                     <label for="school_id">School ID</label>
                     <input type="text" id="school_id" name="school_id" maxlength="11" required>
+                    
+                    <br>
 
                     <label for="current_password">Current Password</label>
                     <input type="password" id="current_password" name="current_password" required>
 
+                    <br>
+
                     <label for="new_password">New Password</label>
                     <input type="password" id="new_password" name="new_password" required>
 
+                    <br>
+
                     <label for="confirm_password">Confirm New Password</label>
                     <input type="password" id="confirm_password" name="confirm_password" required>
+
+                    <br>
 
                     <button type="submit">Change Password</button>
                 </form>
