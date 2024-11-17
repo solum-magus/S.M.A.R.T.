3ae0df42c798d488323a06c27a814d22629b7c2f
solum-magus <stephenchase728@gmail.com>
2024-11-17 23:08:56 +0800

    Update Admin.php

--- S.M.A.R.T/Pages/Admin.php
+++ S.M.A.R.T/Pages/Admin.php
@@ -7,20 +7,23 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../Style/Admin.css">
+        <link rel="stylesheet" href="../Style/Nav_Bar.css">
     </head>
 <body>
         <div class="Container">
             <nav>
                 <svg id="logo" height="50" width="50">
-                    <image height="50" width="50" href="Assets/Logo2.png"></image>
+                    <image height="50" width="50" href="../Assets/Logo2.png"></image>
                 </svg>
                 <span id="smart">SMART</span>
             </nav>
 
             <main>
-			    <div id="mainrep">                    			         
+			    <div id="mainrep">
+                    
 				    <span style="font-size: 30px; 	font-weight: bold; text-align: center;	margin-top: 25px;">Maintanance Report</span>
 			        <table style="background-color: black; width: 95%; height: 95%; margin-right: auto; margin-bottom: 35%;">
+
 					  <tr>
 					    <th>Report ID</th>
 					    <th>Location</th>
@@ -94,34 +97,16 @@
 						     				   
 				   </table>		  							  						
 		    
-						 				   
-				  
                 </div>   
             </main>
              <div id="side">
                 <div id="icons">
-                    <img id="prof" src="Assets/Profile.png">
-                    <img id="home" src="Assets/Home.png">
-                    <img id="id" src="Assets/History.png">
-                    <img id="fb" src="Assets/Feedback.png">
-                    <img id="notif" src="Assets/Notification.png">
+                    <a href="Profile.php"><img id="prof" src="../Assets/Profile.png"></a>
+                    <a href="Homepage.php"><img id="home" src="../Assets/Home.png"></a>
+                    <a href="History.php"><img id="id" src="../Assets/History.png"></a>
+                    <a href="Feedback.php"><img id="fb" src="../Assets/Feedback.png"></a>
+                    <a href="Notification.php"><img id="notif" src="../Assets/Notification.png"></a>
                 </div>
             </div>
- 
-
-
-
-
-
-
-
-
 </body>
-
-
-
-
-
-
-
 </html>
