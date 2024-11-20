d9114cd5f4bf9f8cf005b7ed902ad0f28f34508f
solum-magus <stephenchase728@gmail.com>
2024-11-20 22:05:47 +0800

    Update Feedback.php

--- S.M.A.R.T/Pages/Feedback.php
+++ S.M.A.R.T/Pages/Feedback.php
@@ -2,52 +2,48 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SMART - Feedback</title>
-        <link rel="icon" type="image/x-icon" href="../Assets/Logo2.png">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-        <link rel = "stylesheet" href = "../Style/Feedback.css">
-        <link rel = "stylesheet" href = "../Style/Nav_Bar.css">
+        <title>SMART - Home</title>
+        <link rel = "stylesheet" href = "Feedbackstyle.css">
+		<link rel = "stylesheet" href = "https://fonts.google.com/specimen/Lato?query=lato">
     </head>
-
     <body>
         <div class="Container">
             <nav>
 			    <svg id="logo" height="50" width="50">
-                    <image height="50" width="50" href="../Assets/Logo2.png"></image>
+                    <image height="50" width="50" href="Assets/Logo2.png"></image>
                 </svg>
-                <span id="smart">SMART</span>
+                <span style="color: white; font-size: 64px; font-family: 'Lato'; font-weight: 800;">SMART</span>
 			</nav>
-
             <main>
 			       <div id="nameid">				         
 						       <span style="font-size: 30px; font-weight: bold;">Report ID</span>						  							  						
-						  <input type="text">				   
+						  <input class="input1">				   
 				   </div>
 				   <div id="feedback">
 				        <span style="font-size: 30px; font-weight: bold;">Feedback</span>
-				        <input type="text">
+						
+				        <textarea class="input2"></textarea>
 						<div class="container1">
-    		                <select>
-                                <option value="5">Excellent</option>
-                                <option value="4">Good</option>
-                                <option value="3">Fair</option>
-                                <option value="2">Poor</option>
-                                <option value="1">Very Poor</option>
+    		                
+							<select>
+                                <option value=""disabled selected>Rating</option>							
+                                <option style="color: red">Terrible</option>
+                                <option style="color: orange">Average</option>
+                                <option style="color: green">Excellent</option>							
                             </select>
-                            <button type="Submit">Submit</button>						 
+							
+                        <button>Submit</button>	
+					 
 						</div>
 				   </div>		
-			</main>	
-            			
+			</main>				
             <div id="side">
-			    <div id="icons">
-                    <a href="Profile.php"><img id="prof" src="../Assets/Profile.png"></a>
-                    <a href="Homepage.php"><img id="home" src="../Assets/Home.png"></a>
-                    <a href="History.php"><img id="id" src="../Assets/History.png"></a>
-                    <a href="Feedback.php"><img id="fb" src="../Assets/Feedback.png"></a>
-                    <a href="Notification.php"><img id="notif" src="../Assets/Notification.png"></a>
+			<div id="icons">
+                    <img id="prof" src="Assets/Profile.png">
+                    <img id="home" src="Assets/Home.png">
+                    <img id="id" src="Assets/History.png">
+                    <img id="fb" src="Assets/Feedback.png">
+                    <img id="notif" src="Assets/Notification.png">
                 </div>
 			</div>
 			
