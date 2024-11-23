b46c3e46e22d808fcbfc9ce31c5f63ad026b8454
Zanjoe1 <154309501+Zanjoe1@users.noreply.github.com>
2024-11-23 20:46:41 +0800

    Update Feedback.php

--- S.M.A.R.T/Pages/Feedback.php
+++ S.M.A.R.T/Pages/Feedback.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-if (!isset($_SESSION["position"])){
+if (!isset($_SESSION["position"])) {
     echo "<script>
     alert('You are not logged in!');
     window.location.href = '../index.html';
